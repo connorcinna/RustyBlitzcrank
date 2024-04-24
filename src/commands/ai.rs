e63491290780e85336a48b0b7842bfbb86228e52
connorcinna <connorcinna@gmail.com>
2024-04-23 22:31:10 -0400

    migrated AI command from PaLM from google to locally hosted llm

--- src/commands/ai.rs
+++ src/commands/ai.rs
@@ -10,8 +10,30 @@ use serde_json::json;
 use std::env;
 use reqwest;
 
+//trim some markdown that doesn't work with discord specifically
+pub fn format_string(output: String) -> String
+{
+    //trim the quotation marks
+    let output = &output[1..output.len()-1];
+    //                  this is retarded but it works for discord because it doesn't handle newline
+    //                  characters right
+    let output = output.replace("\\n", " 
+    ");
+    //the model tries to escape all quote characters in code, which is wrong, so
+    //replace those
+    let output = output.replace("\\\"", "\"");
+    //replace tab characters not supported by discord
+    let output = output.replace("\\t", "");
+    let output = output.replace("\\t+", "");
+    output
+}
+
+//connect to the local LLM model running on desktop
 pub async fn run(options: &[CommandDataOption]) -> String {
     dotenv().ok();
+    let model : String = env::var("LLM_MODEL").unwrap();
+    let server_ip : String = env::var("SERVER_IP").unwrap(); 
+    let server_port: String = env::var("SERVER_PORT").unwrap();
     let option = options
         .get(0)
         .expect("Expected prompt option")
@@ -19,50 +41,44 @@ pub async fn run(options: &[CommandDataOption]) -> String {
         .as_ref()
         .expect("Expected prompt option");
     if let CommandDataOptionValue::String(query) = option {
-            let url: String = format!("https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key={}", env::var("GOOGLE_PALM_KEY").expect("Expected a PaLM key"));
+            let url: String = format!("http://{}:{}/v1/chat/completions", server_ip, server_port);
             let client = reqwest::Client::new();
+            let json = &json!(
+            {
+                "model": model,
+                "messages": [ {"role" : "system", "content": query} ],
+                "temperature" : 1,
+                "max_tokens": 1024,
+                "stream" : false,
+            });
             let res = client
                 .post(url.clone())
-                .json(&json!({
-                    "prompt": {
-                        "text": query 
-                    },
-                    "temperature" : 1,
-                    "max_output_tokens": 1024,
-                    //2edgy4me
-                    "safety_settings": [
-                        {"category": "HARM_CATEGORY_DEROGATORY", "threshold":4},
-                        {"category": "HARM_CATEGORY_TOXICITY", "threshold":4},
-                        {"category": "HARM_CATEGORY_VIOLENCE", "threshold":4},
-                        {"category": "HARM_CATEGORY_SEXUAL", "threshold":4},
-                        {"category": "HARM_CATEGORY_MEDICAL", "threshold":4},
-                        {"category": "HARM_CATEGORY_DANGEROUS", "threshold":4},
-                    ],
-                }))
+                .json(json)
                 .send()
                 .await
-                .unwrap();
+                .expect("Unable to connect to LLM server, Connor's desktop probably isn't running");
             match res.status() {
                 reqwest::StatusCode::OK => {
+                    println!("Got statuscode OK");
                     let res_text = res.text().await.unwrap();
+                    println!("{res_text}");
                     let json_result: Value = serde_json::from_str(&res_text).unwrap();
-                    let output = json_result.get("candidates")
+                    println!("{json_result}");
+                    let output = json_result.get("choices")
                         .and_then(|value| value.get(0))
-                        .and_then(|value| value.get("output"))
+                        .and_then(|value| value.get("message"))
+                        .and_then(|value| value.get("content"))
                         .unwrap()
                         .to_string();
                     println!("{:?}", json_result);
-                    let output = output.replace("\\n", " ");
-                    let output = &output[1..output.len()-1];
-                    //println!("Size of output: {}", output.len());
-                    println!("Size of output: {}", output.chars().count());
-                    format!("\n{}", output)
-                    //output
+                    let output = format_string(output);
+                    format!("{}", output)
                 },
                 reqwest::StatusCode::UNAUTHORIZED => {
                     String::from("Error authorizing request")
                 }
                 _ => {
+                    println!("error: got result {} from api", res.status().to_string());
                     panic!("Unexpected error in API response");
                 }
             }
@@ -71,8 +87,6 @@ pub async fn run(options: &[CommandDataOption]) -> String {
         "Please provide a valid query.".to_string()
     }
 }
-
-
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
     command.name("ai").description("Submit a prompt to Google's LLM")
