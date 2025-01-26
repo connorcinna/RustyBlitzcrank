8aaf21dad996e4021557bfd3936428177f7019d0
connorcinna <connorcinna@gmail.com>
2025-01-26 13:29:29 -0500

    think the scheduler works now

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{env, future, pin};
 use dotenv::dotenv;
 use tokio::fs::File;
 use tokio_cron_scheduler::{Job, JobScheduler};
+use chrono::{DateTime, Utc, TimeDelta};
 use poise::serenity_prelude::EventHandler;
 use poise::serenity_prelude as serenity;
 use poise::async_trait;
@@ -19,6 +20,27 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 
 struct Handler;
 struct Data {}
+//TODO move all this extra date and struct stuff somewhere else
+const YEAR_IN_SECONDS: f64 = 60.0 * 60.0 * 24.0 * (365.0 + 0.25);
+const MONTH_IN_SECONDS: f64 = YEAR_IN_SECONDS / 12.0;
+const WEEK_IN_SECONDS: f64 = MONTH_IN_SECONDS / 4.345;
+const DAY_IN_SECONDS: f64 = WEEK_IN_SECONDS / 7.0;
+const HOUR_IN_SECONDS: f64 = DAY_IN_SECONDS / 24.0;
+const MIN_IN_SECONDS: f64 = HOUR_IN_SECONDS / 60.0;
+
+//simple struct to hold break down of TimeDelta
+#[derive(Debug)]
+struct TimeData
+{
+    years: i64,
+    months: i64,
+    weeks: i64,
+    days: i64,
+    hours: i64,
+    minutes: i64,
+    seconds: i64,
+}
+
 
 async fn fix_links(old_link: String, new_link: String, msg: &Message, ctx: &Context)
 {
@@ -72,38 +94,67 @@ impl EventHandler for Handler
     async fn ready(&self, ctx: serenity::Context, ready: serenity::Ready)
     {
         println!("{} is connected!", ready.user.name);
-        ctx.set_activity(Some(poise::serenity_prelude::ActivityData::custom("1459 days of trump left")));
         let channel_id = serenity::ChannelId::new(
                     env::var("MAIN_CHANNEL_ID")
                     .expect("Expected MAIN_CHANNEL_ID in environment")
                     .parse()
                     .expect("MAIN_CHANNEL_ID must be an integer"));
-        begin_scheduled_jobs(channel_id, &ctx);
+        begin_scheduled_jobs(channel_id, ctx);
     }
 }
 
-async fn begin_scheduled_jobs(channel_id: serenity::ChannelId, ctx: &serenity::Context) -> Result<(), Error>
+async fn begin_scheduled_jobs(channel_id: serenity::ChannelId, ctx: serenity::Context) -> Result<(), Error>
 {
-    match JobScheduler::new().await
-    {
-        Ok(schedule) =>
+    let trump_inauguration_date: DateTime<Utc> = DateTime::parse_from_rfc2822("Mon, 20 Jan 2024 12:00:00 -0500").unwrap().to_utc();
+    let schedule = JobScheduler::new().await?;
+    let ctx_clone = ctx.clone();
+    schedule.add(
+        Job::new("0 0 14 * *  Fri", move |_uuid, _l| // 2PM UTC => 9AM EST
         {
-            //async closures don't really work, have to make the inner closure create a future and then
-            //let tokio handle executing it
-            schedule.add(
-                Job::new("0 0 14 * *  Fri *", move |_uuid, _l| // 2PM UTC => 9AM EST
-                {
-                    let http = &ctx.http.clone();
-                    let rt = tokio::runtime::Runtime::new().unwrap();
-                    let builder = serenity::CreateMessage::new().content("https://www.youtube.com/watch?v=WUyJ6N6FD9Q");
-                    let future = channel_id.send_message(http, builder);
-                    let _ = rt.block_on(future);
-                }).unwrap(),
-            ).await.unwrap();
-            return Ok(());
-        },
-        Err(e) =>  { return Err(Error::from(e.to_string())); }
-    };
+            let http = &ctx_clone.http.clone();
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let builder = serenity::CreateMessage::new().content("https://www.youtube.com/watch?v=WUyJ6N6FD9Q");
+            let future = channel_id.send_message(http, builder);
+            let _ = rt.block_on(future);
+        })?);
+    schedule.add(
+        Job::new("1 * * * * *", move |_uuid, _l|
+        {
+            let current_time = chrono::offset::Local::now().to_utc();
+            let td = current_time - trump_inauguration_date;
+            let data = breakdown_time(td);
+            let activity_string = format!("{0} weeks, {1} days, {2} hours, and {3} seconds until Trump's presidency is over", data.weeks, data.days, data.hours, data.seconds);
+            ctx.set_activity(Some(poise::serenity_prelude::ActivityData::custom(activity_string)));
+        })?);
+    schedule.start().await?;
+    return Ok(());
+}
+
+fn breakdown_time(td: TimeDelta) -> TimeData
+{
+    let mut seconds = td.num_seconds();
+    let years = seconds % YEAR_IN_SECONDS as i64;
+    seconds -= YEAR_IN_SECONDS as i64 * years;
+    let months = seconds % MONTH_IN_SECONDS as i64;
+    seconds -= MONTH_IN_SECONDS as i64 * months;
+    let weeks = seconds % WEEK_IN_SECONDS as i64;
+    seconds -= WEEK_IN_SECONDS as i64 * weeks;
+    let days = seconds % DAY_IN_SECONDS as i64;
+    seconds -= DAY_IN_SECONDS as i64 * days;
+    let hours = seconds % HOUR_IN_SECONDS as i64;
+    seconds -= HOUR_IN_SECONDS as i64 * hours;
+    let minutes = seconds % MIN_IN_SECONDS as i64;
+    seconds -= MIN_IN_SECONDS as i64 * minutes;
+    TimeData
+    {
+        years,
+        months,
+        weeks,
+        days,
+        hours,
+        minutes,
+        seconds,
+    }
 }
 
 //handle interactions that require doing some extra stuff other than just sending to the channel
